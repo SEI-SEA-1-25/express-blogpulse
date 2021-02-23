8639463094827c36003f491020c2a908a806c124
kchu01 <chukathy01@gmail.com>
2021-02-22 16:24:48 -0800

    Add Comment

---
+++ dbTest.js
@@ -0,0 +1,11 @@
+const db = require("./models");
+async function dbTest() {
+    const article = await db.article.findOne();
+    const comment = await db.comment.create({
+        name: "Paul Allen",
+        comment: "This is really neat! Thanks for posting.",
+        articleId: article.id,
+    });
+    console.log(comment);
+}
+dbTest();
